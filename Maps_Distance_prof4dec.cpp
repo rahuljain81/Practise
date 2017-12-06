b3ce0cd6d82303035e32cad08db3282cb452433a
rahuljain81 <rahuljain81@gmail.com>
2017-12-06 12:54:56 +0530

    Update Maps_Distance_prof4dec.cpp

--- Maps_Distance_prof4dec.cpp
+++ Maps_Distance_prof4dec.cpp
@@ -1,4 +1,6 @@
-#include <stdio.h>
+//https://swexpertacademy.samsung.com/common/swea/solvingPractice/userProblemDetail.do?contestProbId=AWAgukoPlWQAAAFn&userProblemProcess=&userIsFavorite=&problemTitle=&rowNum=10&pageIndex=1
+
+//#include <stdio.h>
 #define MAX 52
 #define MAX_BUILDING 1001
 
@@ -6,7 +8,7 @@
 #define INTERSECTION 2
 #define PARKING 3
 
-enum 
+enum
 {
 	UP = 0,
 	DOWN = 1,
@@ -23,7 +25,7 @@ typedef struct
 	int px, py;
 }Node;
 
-int map [MAX][MAX];
+int map[MAX][MAX];
 int N;
 
 Node node[MAX_BUILDING];
@@ -37,33 +39,33 @@ typedef struct
 	int steps;
 }BFS;
 
-BFS bfsnode [MAX*MAX*MAX_DIRECTIONS];
+BFS bfsnode[MAX*MAX*MAX_DIRECTIONS];
 
 void init(int n)
 {
 	g_id = 0;
-	cnt=0;
-	N=n+2;
-	for (int i=0; i < N; i++)
-		for (int j=0; j < N; j++)
+	cnt = 0;
+	N = n + 2;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
 			map[i][j] = 0;
 }
 
-void addBuilding(int id, int locX, int locY, int  w, int h, int px, int py)
+void addBuilding(int id, int locY, int locX, int  w, int h, int py, int px)
 {
 	g_id++;
-	printf ("ADD %d: x %d y %d w %d h %d px %d py %d\n", id, locX, locY, w, h, px, py);
+	//printf("ADD %d: x %d y %d w %d h %d px %d py %d\n", id, locY, locX, w, h, py, px);
 	node[id].locx = locX;
 	node[id].locy = locY;
 	node[id].w = w;
 	node[id].h = h;
 	node[id].px = locX + px;
 	node[id].py = locY + py;
-	
 
-	for (int i=0; i < h; i++)
-		for (int j=0; j < w; j++)
-			map[locX + i] [locY + j] = id;//BUILDING;
+
+	for (int i = 0; i < h; i++)
+		for (int j = 0; j < w; j++)
+			map[locX + i][locY + j] = BUILDING;
 
 
 	map[locX + px][locY + py] = PARKING;
@@ -75,44 +77,44 @@ void addBuilding(int id, int locX, int locY, int  w, int h, int px, int py)
 }
 
 
-int issafe (int x, int y)
+int issafe(int x, int y)
 {
 	if (x < 0 || x > N || y < 0 || y > N || map[x][y] == BUILDING || map[x][y] == PARKING)
 		return 0;
 	return 1;
 }
 
-int isclockwise (int x, int y, int dir)
+int isclockwise(int x, int y, int dir)
 {
 	switch (dir)
 	{
-		case UP:
-			if (map[x][y + 1] == 1)
-				return 1;
-			return 0;
-			break;
-		case DOWN:
-			if (map[x][y - 1] == 1)
-				return 1;
-			return 0;
-			break;
-		case LEFT:
-			if (map[x - 1][y] == 1)
-				return 1;
-			return 0;
-			break;
-		case RIGHT:
-			if (map[x + 1][y] == 1)
-				return 1;
-			return 0;
-			break;
-		default:
-			return 0;
+	case UP:
+		if (map[x][y + 1] == BUILDING || map[x][y + 1] == PARKING)
+			return 1;
+		return 0;
+		break;
+	case DOWN:
+		if (map[x][y - 1] == BUILDING || map[x][y - 1] == PARKING)
+			return 1;
+		return 0;
+		break;
+	case LEFT:
+		if (map[x - 1][y] == BUILDING || map[x - 1][y] == PARKING)
+			return 1;
+		return 0;
+		break;
+	case RIGHT:
+		if (map[x + 1][y] == BUILDING || map[x + 1][y] == PARKING)
+			return 1;
+		return 0;
+		break;
+	default:
+		return 0;
 	}
 	return 0;
 }
 
-void enqueue(int x, int y, int dir, int steps, int visited [MAX][MAX][MAX_DIRECTIONS])
+void enqueue(int x, int y, int dir, int steps, int (&visited) [MAX][MAX][MAX_DIRECTIONS])
 {
 	if (!visited[x][y][dir])
 	{
@@ -122,21 +124,22 @@ void enqueue(int x, int y, int dir, int steps, int visited [MAX][MAX][MAX_DIRECT
 		bfsnode[cnt].steps = steps + 1;
 		visited[x][y][dir] = 1;
 		cnt++;
+		//printf("ENQ %d: x %d y %d steps %d dir %d \n", cnt - 1, x, y, steps + 1, dir);
 	}
 }
 
 void dumpmap()
 {
-	for (int i=0; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j=0; j < N; j++)
-			printf ("%d", map[i][j]);
-		printf ("\n");
+		//for (int j = 0; j < N; j++)
+			//printf("%d", map[i][j]);
+		//printf("\n");
 	}
 
-	for (int i=1; i <= g_id; i++)
+	for (int i = 1; i <= g_id; i++)
 	{
-		printf ("%d: x %d y %d w %d h %d \n", node[i].id, node[i].locx, node[i].locy, node[i].w, node[i].h);
+		//printf("%d: x %d y %d w %d h %d \n", node[i].id, node[i].locx, node[i].locy, node[i].w, node[i].h);
 	}
 }
 
@@ -145,62 +148,109 @@ void dumpmap()
 int getDistance(int from, int to)
 {
 	int x, y, dir, steps;
-	int visited[MAX][MAX][MAX_DIRECTIONS];
+	int visited[MAX][MAX][MAX_DIRECTIONS] = { {{0,}} };
 	cnt = 0;
 
-	dumpmap();
+	//printf("Expected To %d (%d,%d) \n", to, node[to].px, node[to].py);
+
+	//dumpmap();
 	//Enqueue 4 Directions
 	{
-		enqueue ( node[from].px, node[from].py + 1, RIGHT, 0, visited);
-		enqueue ( node[from].px, node[from].py - 1, LEFT, 0, visited);
-		enqueue ( node[from].px - 1, node[from].py, UP, 0, visited);
-		enqueue ( node[from].px + 1, node[from].py, DOWN, 0, visited);
+#if 0
+		enqueue(node[from].px, node[from].py + 1, RIGHT, 0, visited);
+		//enqueue(node[from].px, node[from].py - 1, LEFT, 0, visited);
+		enqueue(node[from].px - 1, node[from].py, RIGHT, 0, visited);
+		//enqueue(node[from].px + 1, node[from].py, LEFT, 0, visited);
+#endif
+		x = node[from].px;
+		y = node[from].py;
+		steps = 0;
+
+		if (map[x][y-1] && map[x][y+1] && map[x-1][y])
+			enqueue(x + 1, y - 1, LEFT, 1, visited);
+		else if (map[x][y - 1] && map[x][y + 1] && map[x + 1][y])
+			enqueue(x - 1, y + 1, RIGHT, 1, visited);
+		else if (map[x - 1][y] && map[x + 1][y] && map[x][y + 1])
+			enqueue(x - 1, y - 1, UP, 1, visited);
+		else if (map[x - 1][y] && map[x + 1][y] && map[x][y - 1])
+			enqueue(x + 1, y + 1, DOWN, 1, visited);
 	}
 
 
-	int i = -1;
+	int i = 0;
 	while (i < cnt)
-	{
-		i++;
+	{		
 		x = bfsnode[i].x;
 		y = bfsnode[i].y;
 		dir = bfsnode[i].dir;
 		steps = bfsnode[i].steps;
 
-		if (issafe (x, y) == 0)	
-			continue;
+		//printf("Processing %d \n", i);		
 
-		if (isclockwise (x, y, dir) == 0)
+		i++;
+
+		//Enqueue next valid location from parking lot
+		switch (dir)
+		{
+		case UP:
+			if (x == node[to].px && (y+1) == node[to].py)
+				return steps + 1;			
+			break;
+		case DOWN:
+			if (x == node[to].px && (y - 1) == node[to].py)
+				return steps + 1;
+			break;
+		case LEFT:
+			if ((x - 1) == node[to].px && y == node[to].py)
+				return steps + 1;
+			break;
+		case RIGHT:
+			if ((x + 1) == node[to].px && y == node[to].py)
+				return steps + 1;
+			break;		
+		}
+		
+
+		if (issafe(x, y) == 0)
 			continue;
 
 		if (map[x][y] == INTERSECTION)
 		{
 			//Enqueue 4 Directions
-			enqueue ( x, y + 1, RIGHT, steps, visited);
-			enqueue ( x, y - 1, LEFT, steps, visited);
-			enqueue ( x - 1, y, UP, steps, visited);
-			enqueue ( x + 1, y, DOWN, steps, visited);
+			//printf("REACHED INTERSECTION\n");
+			enqueue(x, y + 1, RIGHT, steps, visited);
+			enqueue(x, y - 1, LEFT, steps, visited);
+			enqueue(x - 1, y, UP, steps, visited);
+			enqueue(x + 1, y, DOWN, steps, visited);
 		}
 		else
 		{
+			if (isclockwise(x, y, dir) == 0)
+				continue;
+#if 0
+			enqueue(x, y + 1, RIGHT, steps, visited);
+			enqueue(x + 1, y, DOWN, steps, visited);
+			enqueue(x, y - 1, LEFT, steps, visited);
+			enqueue(x - 1, y, UP, steps, visited);
+#else
 			//enqueue respective direction
 			switch (dir)
 			{
-				case RIGHT:
-				case DOWN:
-					enqueue ( x, y + 1, RIGHT, steps, visited);
-					enqueue ( x + 1, y, DOWN, steps, visited);
-					break;
-				case LEFT:
-				case UP:
-					enqueue ( x, y - 1, LEFT, steps, visited);
-					enqueue ( x - 1, y, UP, steps, visited);
-					break;
+			case RIGHT:
+				enqueue(x, y + 1, RIGHT, steps, visited);
+				break;
+			case DOWN:				
+				enqueue(x + 1, y, DOWN, steps, visited);				
+				break;
+			case LEFT:
+				enqueue(x, y - 1, LEFT, steps, visited);
+				break;
+			case UP:
+				enqueue(x - 1, y, UP, steps, visited);				
+				break;
 			}
-		}
-
-		if (bfsnode[i].x == node[to].px && bfsnode[i].y == node[to].py)
-			return bfsnode[i].steps;
+#endif
+		}		
 	}
 
 
